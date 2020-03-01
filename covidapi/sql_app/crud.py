1e462ae221bade23c4721626533c1b416f701311
Andrea Grandi <a.grandi@gmail.com>
2020-03-01 14:07:36 +0100

    Add initial CRUD operations for DailyReport

---
+++ covidapi/sql_app/crud.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session
+
+from . import models
+
+
+def get_daily_report(db: Session, daily_report_id: int):
+    return db.query(models.DailyReport).filter(models.DailyReport.id == daily_report_id).first()
+
+
+def get_daily_reports(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.DailyReport).offset(skip).limit(limit).all()
