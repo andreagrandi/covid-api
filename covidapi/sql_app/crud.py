6f3723b87d3af94d78fde01358d61181ef8e6c9d
Andrea Grandi <a.grandi@gmail.com>
2020-03-01 14:24:55 +0100

    Add create_daily_report CRUD method

--- covidapi/sql_app/crud.py
+++ covidapi/sql_app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from . import models
+from . import models, schemas
 
 
 def get_daily_report(db: Session, daily_report_id: int):
@@ -13,3 +13,20 @@ def get_daily_report(db: Session, daily_report_id: int):
 
 def get_daily_reports(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.DailyReport).offset(skip).limit(limit).all()
+
+
+def create_daily_report(db: Session, daily_report: schemas.DailyReport):
+    db_daily_report = models.DailyReport(
+        country_region=daily_report.country_region,
+        province_state=daily_report.province_state,
+        last_update=daily_report.last_update,
+        confirmed=daily_report.confirmed,
+        deaths=daily_report.deaths,
+        recovered=daily_report.recovered,
+    )
+
+    db.add(db_daily_report)
+    db.commit()
+    db.refresh(db_daily_report)
+
+    return db_daily_report
