e7f169aa2ae4f40bfb7f4788f611c7ac7bba4a4f
shivanshug14 <shivanshug14@gmail.com>
2020-03-22 18:37:28 +0530

    Fixed requirements.txt, added missing file, removed redundant code

--- covidapi/api/views.py
+++ covidapi/api/views.py
@@ -1,7 +1,6 @@
 import pkg_resources
 from fastapi import Depends
 from fastapi import APIRouter
-from starlette.responses import RedirectResponse
 from starlette.responses import HTMLResponse
 from typing import List
 
@@ -18,7 +17,6 @@ def root():
 @router.get('/v1/health')
 def health():
     return {'status': 'ok'}
-
 
 @router.get("/v1/daily-reports/", response_model=List[DailyReport])
 def get_daily_reports(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
--- covidapi/app.py
+++ covidapi/app.py
@@ -1,12 +1,5 @@
 from fastapi import FastAPI
 from .api import views
-from starlette.staticfiles import StaticFiles
-from pathlib import Path
-
-current_file = Path(__file__)
-parent_dir = current_file.parent
-static_dir = f"{parent_dir.resolve()}/api/static"
 
 app = FastAPI()
-app.mount("/index.html", StaticFiles(directory=static_dir), name="static")
 app.include_router(views.router)
