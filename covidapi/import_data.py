10299dfad1685214a3bcbf5cb09cb4f0c8e4cff2
Mat Moore <matmoore@users.noreply.github.com>
2020-03-28 15:21:45 +0000

    Make the import script idempotent
    Before inserting a daily report, check if the daily report already exists.
    If it exists, but one of the values is different, update them to match the CSV.
    
    As of today, there are 3430 regions per daily report, so we're executing 6860 queries
    per day we want to import. This takes about 35 seconds to run.

--- covidapi/import_data.py
+++ covidapi/import_data.py
@@ -1,7 +1,8 @@
 from db.models import DailyReport
 from db.database import SessionLocal, engine, Base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
 from datetime import datetime
-from pathlib import Path
 from dataflows import Flow, load, checkpoint
 
 BASE_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
@@ -16,6 +17,28 @@ def get_data_with_caching(date_string):
     return result[0][0]
 
 
+def get_daily_report_by_region_and_date(db: Session, country_region: str, province_state: str, last_update: datetime) -> DailyReport:
+    """
+    Get a single daily report from the db by matching the region and date.
+
+    Note that province_state is null for some values of country_region.
+    """
+    if province_state:
+        dr = db.query(DailyReport).filter(
+            DailyReport.province_state == province_state,
+            DailyReport.country_region == country_region,
+            DailyReport.last_update == last_update
+        )
+    else:
+        dr = db.query(DailyReport).filter(
+            DailyReport.province_state.is_(None),
+            DailyReport.country_region == country_region,
+            DailyReport.last_update == last_update
+        )
+
+    return dr.one()
+
+
 def main():
     print("Importing data into db")
 
@@ -23,14 +46,30 @@ def main():
     db_instance = SessionLocal()
 
     for row in get_data_with_caching('02-29-2020'):
-        dr = DailyReport(
-            province_state=row['Province/State'],
-            country_region=row['Country/Region'],
-            last_update=datetime.fromisoformat(row['Last Update']),
-            confirmed=row['Confirmed'],
-            deaths=row['Deaths'],
-            recovered=row['Recovered'],
-        )
+        province_state = row['Province/State']
+        country_region = row['Country/Region']
+        last_update = datetime.fromisoformat(row['Last Update'])
+
+        try:
+            dr = get_daily_report_by_region_and_date(
+                db=db_instance,
+                province_state=province_state,
+                country_region=country_region,
+                last_update=last_update
+            )
+        except NoResultFound:
+            dr = DailyReport(
+                province_state=province_state,
+                country_region=country_region,
+                last_update=last_update,
+                confirmed=row['Confirmed'],
+                deaths=row['Deaths'],
+                recovered=row['Recovered'],
+            )
+        else:
+            dr.confirmed = row['Confirmed']
+            dr.deaths = row['Deaths']
+            dr.recovered = row['Recovered']
 
         db_instance.add(dr)
 
