280722b1394d78823d817c4ccae0b3f05969cccc
Andrea Grandi <a.grandi@gmail.com>
2020-04-01 23:41:57 +0200

    Refactor existing code to support multiple data sources (fixes #39)

--- covidapi/services/crud.py
+++ covidapi/services/crud.py
@@ -4,30 +4,29 @@ from ..db import models
 from ..schemas import schemas
 
 
-def get_daily_report(db: Session, daily_report_id: int):
-    return db.query(
-            models.DailyReport
-        ).filter(
-            models.DailyReport.id == daily_report_id
-        ).first()
+class JHCRUD:
+    def get_daily_report(self, db: Session, daily_report_id: int):
+        return db.query(
+                models.JHDailyReport
+            ).filter(
+                models.JHDailyReport.id == daily_report_id
+            ).first()
 
+    def get_daily_reports(self, db: Session, skip: int = 0, limit: int = 100):
+        return db.query(models.JHDailyReport).offset(skip).limit(limit).all()
 
-def get_daily_reports(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.DailyReport).offset(skip).limit(limit).all()
+    def create_daily_report(self, db: Session, daily_report: schemas.JHDailyReport):
+        db_daily_report = models.JHDailyReport(
+            country_region=daily_report.country_region,
+            province_state=daily_report.province_state,
+            last_update=daily_report.last_update,
+            confirmed=daily_report.confirmed,
+            deaths=daily_report.deaths,
+            recovered=daily_report.recovered,
+        )
 
+        db.add(db_daily_report)
+        db.commit()
+        db.refresh(db_daily_report)
 
-def create_daily_report(db: Session, daily_report: schemas.DailyReport):
-    db_daily_report = models.DailyReport(
-        country_region=daily_report.country_region,
-        province_state=daily_report.province_state,
-        last_update=daily_report.last_update,
-        confirmed=daily_report.confirmed,
-        deaths=daily_report.deaths,
-        recovered=daily_report.recovered,
-    )
-
-    db.add(db_daily_report)
-    db.commit()
-    db.refresh(db_daily_report)
-
-    return db_daily_report
+        return db_daily_report
